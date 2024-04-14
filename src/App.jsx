2743d2e7d63ae988783c85d460983f10377f0982
Aniket <aniketraj28042003@gmail.com>
2024-04-14 16:24:08 +0530

    added post card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import Button from '@mui/material/Button';
 
+import PostCard from './components/PostCard';
+
 function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <>
+      <PostCard />
+    </>
+  )
 }
 
 export default App
